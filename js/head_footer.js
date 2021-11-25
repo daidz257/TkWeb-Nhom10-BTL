7b1b2fbc1657c13f22f24707cf9a5280b4ba9170
maiphuong0903 <93458683+maiphuong0903@users.noreply.github.com>
2021-11-25 19:17:36 +0700

    Add files via upload

---
+++ js/head_footer.js
@@ -0,0 +1,8 @@
+function get(){
+    var gui=document.getElementById("ghichu").value;
+    if( gui == "" || gui == Null)
+    {
+       alert("Vui lòng điền nội dung cần phản hồi!");
+       return false;
+    }
+}
